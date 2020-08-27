e125f759654d62d29cb3a70f2888cec16e1e6778
Leewansze <1226186467@qq.com>
2020-08-27 20:55:07 +0800

    [JS-refactor][alice]refactor:extract getTextResult method and the for-loop in the caculateCredits method

--- src/statement.js
+++ src/statement.js
@@ -1,25 +1,27 @@
 function statement(invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    let volumeCredits = 0;
-    let totalAmount = 0;
-    const format = formatUsd();
-    for (let perf of invoice.performances) {
-        const play = getPlaysId(plays, perf);
-        let thisAmount = caculateAmount(perf, play);
-        // add volume credits
-        volumeCredits += caculateCredits(perf, play);
-        //print line for this order
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
-    }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits \n`;
-    return result;
+    return getTextResult(invoice, plays);
 }
 
 module.exports = {
     statement,
 };
+
+function getTextResult(invoice, plays) {
+    let volumeCredits = 0;
+    let totalAmount = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = formatUsd();
+    for (let perf of invoice.performances) {
+        const play = getPlaysId(plays, perf);
+        let thisAmount = caculateAmount(perf, play);
+        //print line for this order
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${caculateCredits(invoice, plays)} credits \n`;
+    return result;
+}
 
 function caculateAmount(perf, play) {
     let thisAmount = 0;
@@ -57,12 +59,14 @@ function tragedyStrategy(perf) {
     return thisAmount;
 }
 
-function caculateCredits(perf, play) {
-    let credit = Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type)
-        credit += Math.floor(perf.audience / 5);
-    return credit;
+function caculateCredits(invoice, plays) {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ('comedy' === plays[perf.playID].type)
+            volumeCredits += Math.floor(perf.audience / 5);
+    }
+    return volumeCredits;
 }
 
 function formatUsd() {
@@ -71,4 +75,16 @@ function formatUsd() {
         currency: 'USD',
         minimumFractionDigits: 2,
     }).format;
+}
+
+function getHtmlResult(invoice, plays) {
+    var result = '<h1>Statement for BigCo</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n'
 }
